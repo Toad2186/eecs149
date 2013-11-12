0188b51778b4296340ddc8619a37aff942d436ee
Toan Vuong <tvuong@toanvuong.com>
2013-11-12 08:35:12 -0800

    Should work with CCS now

---
+++ project/CCS/2231_ADC/Debug/adc.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+adc.obj: ../adc.c
+adc.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h
+adc.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+adc.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+adc.obj: ../decl.h
+adc.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdbool.h
+adc.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/yvals.h
+adc.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h
+adc.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
+adc.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/_lock.h
+
+../adc.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+../decl.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/_lock.h: 
---
+++ project/CCS/2231_ADC/Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2231.cmd 
+
+ASM_SRCS += \
+../serial.asm 
+
+C_SRCS += \
+../adc.c \
+../main.c \
+../printf.c 
+
+OBJS += \
+./adc.obj \
+./main.obj \
+./printf.obj \
+./serial.obj 
+
+ASM_DEPS += \
+./serial.pp 
+
+C_DEPS += \
+./adc.pp \
+./main.pp \
+./printf.pp 
+
+C_DEPS__QUOTED += \
+"adc.pp" \
+"main.pp" \
+"printf.pp" 
+
+OBJS__QUOTED += \
+"adc.obj" \
+"main.obj" \
+"printf.obj" \
+"serial.obj" 
+
+ASM_DEPS__QUOTED += \
+"serial.pp" 
+
+C_SRCS__QUOTED += \
+"../adc.c" \
+"../main.c" \
+"../printf.c" 
+
+ASM_SRCS__QUOTED += \
+"../serial.asm" 
+
+
